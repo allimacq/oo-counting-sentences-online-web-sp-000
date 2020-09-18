5c623182dac45cb527643e56c9c7523b27967aa7
Allison MacQueen <allimacq@gmail.com>
2020-09-18 22:26:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,8 @@ class String
   end
 
   def count_sentences
-    #string_split = self.split
-    self.split.scan(/[\.?!]/)
+    string_split = self.split
+    string_split.is_a?(Array)
+    #.scan(/[\.?!]/)
   end
 end
